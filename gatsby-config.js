f73430501224f52688c22bbc9a8728a16d95c767
JBattishill <jbattishill91@gmail.com>
2022-08-23 17:00:37 +1000

    final netlify cms removal

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,6 @@
 module.exports = {
   plugins: [
     {
-      plugins: [`gatsby-plugin-netlify-cms`],
       resolve: `gatsby-theme-codebushi`,
       options: {
         tailwindConfig: `tailwind.config.js`
