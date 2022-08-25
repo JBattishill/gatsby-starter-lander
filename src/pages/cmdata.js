0c0fe7769b129cc52fe300df42c0f3509a1e0e26
JBattishill <jbattishill91@gmail.com>
2022-08-25 11:02:59 +1000

    connecting to API

---
+++ src/pages/cmdata.js
@@ -0,0 +1,49 @@
+import React from "react";
+import './App.css';
+class App extends React.Component {
+   
+    // Constructor 
+    constructor(props) {
+        super(props);
+   
+        this.state = {
+            items: [],
+            DataisLoaded: false
+        };
+    }
+   
+    // ComponentDidMount is used to
+    // execute the code 
+    componentDidMount() {
+        fetch(
+"http://cm.beneb.com/wp-json/wp/v2/pages/")
+            .then((res) => res.json())
+            .then((json) => {
+                this.setState({
+                    items: json,
+                    DataisLoaded: true
+                });
+            })
+    }
+    render() {
+        const { DataisLoaded, items } = this.state;
+        if (!DataisLoaded) return <div>
+            <h1> Pleses wait some time.... </h1> </div> ;
+   
+        return (
+        <div className = "App">
+            <h1> Fetch data from an api in react </h1>  {
+                items.map((item) => ( 
+                <ol key = { item.id } >
+                    User_Name: { item.slug }, 
+                    Full_Name: { item.title.rendered}, 
+                    User_Email: { item.email } 
+                    </ol>
+                ))
+            }
+        </div>
+    );
+}
+}
+   
+export default App;
